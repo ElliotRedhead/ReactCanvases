d9a907eeaea915347e91ade7438d61f7604ac303
ElliotRedhead <elliot.redhead@gmail.com>
2021-09-04 16:40:40 +0100

    Variable opacity

--- src/App.js
+++ src/App.js
@@ -10,41 +10,50 @@ const App = () => {
 		return vector;
 	};
 
-	const numberNodes = 100;
+	const numberNodes = 50;
 	const nodeAttrs = [];
 	for (let i=0; i < numberNodes; i++){
-		const randomX = Math.floor(Math.random()*1300) + 100;
-		const transformX = 0.5*Math.random()*0.01;
+		const randomX = Math.floor(Math.random()*1300) + 50;
+		const transformX = Math.random()*0.001;
 		const transformXDirection = randomDirection();
 		const randomY = Math.floor(Math.random()*500) + 100;
-		const transformY = Math.random()*0.01;
+		const transformY = Math.random()*0.001;
 		const transformYDirection = randomDirection();
-		const rateGrowth = Math.random()*0.01 + 0.002;
-		nodeAttrs.push({xOrd: randomX, yOrd: randomY, rateGrowth, transformX, transformXDirection, transformY, transformYDirection});
+		const rateGrowth = Math.random()*0.005;
+		const opacity = Math.random()*0.005;
+		const opacityChange = Math.random()*0.001;
+		const opacityDirection = randomDirection();
+		nodeAttrs.push({xOrd: randomX, yOrd: randomY, rateGrowth, transformX, transformXDirection, transformY, transformYDirection, opacity, opacityChange, opacityDirection});
 	}
-	console.log(nodeAttrs);
 	const draw = (ctx, frameCount) => {
 		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-		ctx.fillStyle = '#fff'; //white
 
 		for (let i=0; i < numberNodes; i++){
+			let opacity;
+			if (nodeAttrs[i].opacityDirection){
+				opacity = Math.abs(nodeAttrs[i].opacity + Math.sin(frameCount * nodeAttrs[i].opacityChange));
+			} else {
+				opacity = Math.abs(nodeAttrs[i].opacity - Math.sin(frameCount * nodeAttrs[i].opacityChange));
+			}
+			ctx.fillStyle = `rgba(255,255,255,${opacity})`;
+			
 			ctx.beginPath();
 			let xPath;
 			let yPath;
 
 			if (nodeAttrs[i].transformXDirection){
-				xPath = nodeAttrs[i].xOrd + 100*Math.sin(frameCount * nodeAttrs[i].transformX);
+				xPath = nodeAttrs[i].xOrd + 500*Math.sin(frameCount * nodeAttrs[i].transformX)**2;
 			} else {
-				xPath = nodeAttrs[i].xOrd - 100*Math.sin(frameCount * nodeAttrs[i].transformX);
+				xPath = nodeAttrs[i].xOrd - 500*Math.sin(frameCount * nodeAttrs[i].transformX)**2;
 			}
 
 			if (nodeAttrs[i].transformYDirection){
-				yPath = nodeAttrs[i].yOrd + 100*Math.sin(frameCount * nodeAttrs[i].transformX);
+				yPath = nodeAttrs[i].yOrd + 500*Math.sin(frameCount * nodeAttrs[i].transformY)**2;
 			} else {
-				yPath = nodeAttrs[i].yOrd - 100*Math.sin(frameCount * nodeAttrs[i].transformX);
+				yPath = nodeAttrs[i].yOrd - 500*Math.sin(frameCount * nodeAttrs[i].transformY)**2;
 			}
 
-			const growth = 10*Math.sin(frameCount*nodeAttrs[i].rateGrowth)**2 + 5;
+			const growth = Math.sin(frameCount*nodeAttrs[i].rateGrowth)**2 + 30;
 			ctx.arc(xPath, yPath, growth, 0, 2*Math.PI);
 			ctx.fill();
 		}
